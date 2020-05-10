7bbb5a8324b4364c48692462714ec7246e2f590d
atasoyfurkan <=>
2020-05-10 19:44:07 +0300

    Updates

---
+++ precache-manifest.d938e13a5fca867bf5dd7bb1af34977d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeb203039214dbdfcc9d1810cced3d91",
+    "url": "/song-recommender/index.html"
+  },
+  {
+    "revision": "6928b04887c1d72ccfaf",
+    "url": "/song-recommender/static/css/2.6a67ccd3.chunk.css"
+  },
+  {
+    "revision": "55c89d8bc2997850fb96",
+    "url": "/song-recommender/static/css/main.07de19ae.chunk.css"
+  },
+  {
+    "revision": "6928b04887c1d72ccfaf",
+    "url": "/song-recommender/static/js/2.4e86d964.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/song-recommender/static/js/2.4e86d964.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55c89d8bc2997850fb96",
+    "url": "/song-recommender/static/js/main.21704400.chunk.js"
+  },
+  {
+    "revision": "39dd1e9377071522f672",
+    "url": "/song-recommender/static/js/runtime-main.76934b7d.js"
+  },
+  {
+    "revision": "50a4ab76e700a83e649be213f820fbbd",
+    "url": "/song-recommender/static/media/icofont.50a4ab76.woff2"
+  },
+  {
+    "revision": "f6ab04aed30a8643bf94fe00f7ff0b59",
+    "url": "/song-recommender/static/media/icofont.f6ab04ae.woff"
+  }
+]);
